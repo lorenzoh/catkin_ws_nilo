598cad8d92ec1c74f7ad95cdf3a4bb5aee453857
Nilshut <nilshut@web.de>
2019-12-09 12:39:07 +0100

    update A8

--- src/assignment8/src/calc_odometry.py
+++ src/assignment8/src/calc_odometry.py
@@ -5,25 +5,33 @@ import csv
 import math
 import sys
 import numpy as np
+import tf
+
 import rospy
 from autominy_msgs.msg import NormalizedSteeringCommand, SpeedCommand, SteeringFeedback , Tick, SteeringAngle, Speed
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, Quaternion, Point, Twist, Vector3
+
 #from autominy_msgs.mgs import SteeringAngle
 # from nav_msgs.msg import Odometry
 # from std_msgs.msg import Float32, String
 
 class Timer:
     def __index__(self):
-        self.start_time = time.time()
+        self.start_time = rospy.Time.now()
     def start_timer(self):
-        self.start_time = time.time()
+        self.start_time = rospy.Time.now()
     def stop_timer(self):
-        return time.time() - self.start_time
+        return (rospy.Time.now() - self.start_time).to_sec()
 
-x_pos = 0
-y_pos = 0
-steering = 0
+x_pos = 0.0
+y_pos = 0.0
+steering = 0.0
 last_steering = 0
 timer = Timer()
+
+publisher_calc_odom = None
+odom_broadcaster = None
 
 def callback_speed(data):
     global last_steering, timer
@@ -31,19 +39,20 @@ def callback_speed(data):
     if(last_steering != None):
         delta_time = timer.stop_timer()
         timer.start_timer()
-        print(delta_time)
+        #print(delta_time)
         calc_next_pos(speed,last_steering,delta_time)
-    #print('speed: '+ str(speed))
+    # print('speed: '+ str(speed))
 
 def callback_steering(data):
     global last_steering
     steering = data.value
     last_steering = steering
-    #print('steering angle: '+ str(steering))
+    # print('steering angle: '+ str(steering))
 
-def callback_gps(self, data):
+def callback_gps_cam(data):
     x = data.pose.pose.position.x
     y = data.pose.pose.position.y
+    # print("cam_x: "+ str(x) + ", y_cam: " + str(y))
 
 def calc_next_pos(speed,steering_angle,delta_time):
     global last_steering, x_pos, y_pos, steering
@@ -55,22 +64,42 @@ def calc_next_pos(speed,steering_angle,delta_time):
     current_x = speed * math.cos(current_steering)
     current_y = speed * math.sin(current_steering)
 
-    #calc new pos
+    # calc new pos
     steering = steering + delta_time * current_steering
     x_pos = x_pos + delta_time * current_x
     y_pos = y_pos + delta_time * current_y
 
+    # pub calc odom
+    current_time = rospy.Time.now()
+
+    odom = Odometry()
+    odom.header.frame_id = "map"
+    odom.header.stamp = current_time
+
+    # set the position
+    odom_quat = tf.transformations.quaternion_from_euler(0, 0, steering)
+    odom.pose.pose = Pose(Point(x_pos, y_pos, 0.), Quaternion(*odom_quat))
+
+    # set the velocity
+    odom.child_frame_id = "base_link"
+    #odom.twist.twist = Twist(Vector3(speed, vy, 0), Vector3(0, 0, vth)) TODO
+    odom.twist.twist = Twist(Vector3(speed, 0, 0), Vector3(0, 0, current_steering))
+
+    # publish the message
+    publisher_calc_odom.publish(odom)
+
+
 def main():
     rospy.init_node("odometry_node")
     timer.start_timer()
-    subscriber_steering = rospy.Subscriber(
-        "/sensors/steering", SteeringAngle, callback_steering)
-    subscriber_speed = rospy.Subscriber(
-        "/sensors/speed", Speed, callback_speed)
+    subscriber_steering = rospy.Subscriber("/sensors/steering", SteeringAngle, callback_steering)
+    subscriber_speed = rospy.Subscriber("/sensors/speed", Speed, callback_speed)
+    subscriber_gps_cam = rospy.Subscriber("/communication/gps/16", Odometry, callback_gps_cam)
+    global publisher_calc_odom
+    global odom_broadcaster
+    publisher_calc_odom = rospy.Publisher("calc_odom", Odometry, queue_size=100)
+    odom_broadcaster = tf.TransformBroadcaster()
 
-    # subscriber_gps = rospy.Subscriber(
-    #     "/communication/gps/10", Odometry, callback_gps)
-    #
     # publisher_speed = rospy.Publisher(
     #     "/actuators/speed", SpeedCommand, queue_size=100)
     #
